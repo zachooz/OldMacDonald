924a0cc7a05fbaf09ba0f7dd43e2a0f59f52a74f
zachooz <hoffking@gmail.com>
2014-10-22 10:35:53 -0700

    initial commit

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 /* @pjs preload="chicken.png; cow.png; pig.png; poop.png; grass.png;*/
 PImage cow;
 PImage pig;
 PImage chicken;
 PImage poop;
 PImage grass;
-void setup(){
-	size(800,800);
+public void setup(){
+	size(1000,1000);
 	cow = loadImage("cow.png");
 	pig = loadImage("pig.png");
 	chicken = loadImage("chicken.png");
@@ -108,6 +124,15 @@ class Poop{
 		this.y=y;
 	}
 }
-void draw(){
+public void draw(){
 	image(grass, 0, 0, 1024, 1024);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
