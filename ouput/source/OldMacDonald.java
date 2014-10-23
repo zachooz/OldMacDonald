f6bad4484b54346483444c785e85174d3262b29b
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2014-10-22 18:12:52 -0700

    Farm finished

--- ouput/source/OldMacDonald.java
+++ ouput/source/OldMacDonald.java
@@ -1,5 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.Arrays; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 /* @pjs preload="chicken.png; cow.png; pig.png; poop.png; grass.png;*/
-import java.util.Arrays;
+
 
 PImage aCow;
 PImage aPig;
@@ -9,7 +27,7 @@ PImage grass;
 Farm theFarm;
 Poop[] poopHolder;
 int poopCount;
-void setup(){
+public void setup(){
 	size(800,800);
 	aCow = loadImage("cow.png");
 	aPig = loadImage("pig.png");
@@ -164,9 +182,18 @@ class Poop{
 		theHeight = 60;
 	}
 }
-void draw(){
+public void draw(){
 	imageMode(CORNER);
 	image(grass, 0, 0, 1024, 1024);
 	imageMode(CENTER);
 	theFarm.run();
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
